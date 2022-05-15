44fd9c912e243f935020cb97262da1292d3cb10f
mwazir <mwazir1@hotmail.com>
2022-05-15 12:51:12 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBimxIP4gXRlBcDPhuTQMSy9I94AZBBY1U",
+    authDomain: "shopify-intern-challenge-2022.firebaseapp.com",
+    projectId: "shopify-intern-challenge-2022",
+    storageBucket: "shopify-intern-challenge-2022.appspot.com",
+    messagingSenderId: "1083401652431",
+    appId: "1:1083401652431:web:401aac66dc334590d50fba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
